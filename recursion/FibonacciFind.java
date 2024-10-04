cd4b27fb0daca48bc41fbab40013e64c711cb735
Omkarrrrrrr <ominalawade77@gmail.com>
2024-10-04 16:19:09 +0530

    Fibonacci series

---
+++ recursion/FibonacciFind.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FibonacciFind{
+
+    public static int FibonacciFind(int n){
+
+        int result = 0;
+        if(n == 1 || n == 0){
+            return n;
+        }
+        else{
+            result = FibonacciFind(n-1)+FibonacciFind(n-2);
+        }
+        return result;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the number in fibonacci series: ");
+        int n = sc.nextInt();
+
+        int result = FibonacciFind(n);
+        System.out.println("the final output is : " + result);
+    }
+    
+}
+
+// Time complexity = O(2^n)
+// Space complexity = O(n)
---
+++ recursion/FibonacciFind2.java
@@ -0,0 +1,32 @@
+// here we will be finding using for loop
+
+import java.util.*;
+
+public class FibonacciFind2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number here: ");
+        int n = sc.nextInt();
+
+        if(n<=0){
+            System.out.println("No must be greater than zero");
+            return ;       //program terminates if the input is invalid.
+        }
+
+        int a = 0,b =1;
+        
+        for(int i=0;i<=n;i++){
+            if(i == 0){
+                System.out.print("fibonacci series is : " + a);
+            }
+            else{
+                System.out.print(","+ b);
+                int next = a + b;
+                a = b;
+                b = next;
+            }
+        }
+        
+    }
+    
+}
