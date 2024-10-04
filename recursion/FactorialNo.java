3c7a2829653eafe051533e587691594cf96c10cb
Omkarrrrrrr <ominalawade77@gmail.com>
2024-10-04 12:50:16 +0530

    DSA_Practice

---
+++ recursion/FactorialNo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class FactorialNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+
+        //FunctionCalling
+        int result = factorialNum(n);
+        System.out.println(result);
+        ;
+
+    }
+
+    private static int factorialNum(int n) {
+        if (n == 0 || n == 1) {
+            return 1;                                       //base case condition
+        } else {
+            int result = n * (factorialNum(n - 1));         //Recursive function calling
+            return result;
+        }
+
+    }
+}
+
+//Time complexity = O(n)
+//Space complexity = O(n)
---
+++ recursion/FactorialNo2.java
@@ -0,0 +1,23 @@
+
+//Using iterative approach - for loop
+import java.util.*;
+public class FactorialNo2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number here: ");
+        int n = sc.nextInt();
+
+        System.out.println("The factorial of number " + n + "is: " + factorialNum(n));
+    }
+
+    private static int factorialNum(int n) {
+
+        int result =1;         //This ensures that the first multiplication works correctly since multiplying by 1 does not change the result.
+
+        for(int i=1;i<=n;i++){
+            result *= i;  // Multiply result by each number from 1 to n
+        }
+        
+        return result;
+    }
+}
