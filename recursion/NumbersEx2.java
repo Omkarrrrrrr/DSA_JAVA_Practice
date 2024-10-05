f5e3325ca29bcc48e9576e71c11312bce38b5c5e
Omkarrrrrrr <ominalawade77@gmail.com>
2024-10-05 11:14:34 +0530

    Recursion basic implementation

---
+++ recursion/NumbersEx2.java
@@ -0,0 +1,17 @@
+public class NumbersEx2 {
+    public static void main(String[] args) {
+      // Print numbers from 1 to 5 using recursion
+        print(1);  
+     
+}
+
+    static void print(int n) {
+        if (n > 5) { // Base case: stop recursion when n is greater than 5
+            return;
+        }
+        System.out.println(n); // Print the current number
+        print(n + 1); // Recursive call to print the next number
+       
+    }
+    
+}
